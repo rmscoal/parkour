c9ec8a12a8f91df092522186d99c9f474c86d3f0
rmscoal <rifky.m.s@mail.ugm.ac.id>
2022-12-16 00:54:01 +0700

    Finish unregister parking controller

--- src/controllers/parking.controller.js
+++ src/controllers/parking.controller.js
@@ -2,20 +2,38 @@ const httpStatus = require('http-status');
 const validateRequest = require('../utils/validateRequest');
 const catchAsync = require('../utils/catchAsync');
 const { parkingUseCase } = require('../usecases');
+const config = require('../config/config');
 
 const registerParking = catchAsync(async (req, res) => {
   await validateRequest(req, res);
   const data = await parkingUseCase.newParking(req.body);
 
-  return res.status(httpStatus.CREATED).send(data);
+  return res.status(httpStatus.CREATED).json({
+    message: 'Enjoy your parking',
+    data: {
+      id: data.id,
+      vech_type: data.vech_type,
+      vech_num: data.vech_num,
+      in_time: data.in_time.toLocaleString('sv', { timeZone: config.timeZone }),
+    },
+  });
 });
 
-const unregisterParking = async (req, res) => {
+const unregisterParking = catchAsync(async (req, res) => {
   await validateRequest(req, res);
+  const data = await parkingUseCase.unregisterParking(req.body);
   return res.status(httpStatus.OK).json({
-    meessage: 'hello',
+    meessage: 'Thank you for parking with us',
+    data: {
+      id: data.id,
+      vech_type: data.vech_type,
+      vech_num: data.vech_num,
+      in_time: data.in_time.toLocaleString('sv', { timeZone: config.timeZone }),
+      out_time: data.out_time.toLocaleString('sv', { timeZone: config.timeZone }),
+      total: data.total,
+    },
   });
-};
+});
 
 module.exports = {
   registerParking,
--- src/usecases/parking.usecase.js
+++ src/usecases/parking.usecase.js
@@ -1,23 +1,62 @@
 /* eslint-disable no-param-reassign */
+const httpStatus = require('http-status');
 const config = require('../config/config');
-const { parking } = require('../db/models');
+const { Parking } = require('../db/models');
+const ApiError = require('../utils/ApiError');
+const parkingPaymentCalculator = require('../utils/parkingPaymentCalculator');
+
+/**
+ * Finds the vehicle that is still parking
+ * @param {String} vechNumber
+ * @returns {Promise<Parking>}
+ */
+const findVehicleInParking = async (vechNumber) => Parking.findOne({
+  where: {
+    vech_num: vechNumber,
+    out_time: null,
+  },
+});
 
 /**
  * Create a new parking
  * @param {Object} parkingData
- * @returns {Promise<parking>}
+ * @returns {Promise<Parking>}
  */
-
 const newParking = async (parkingData) => {
   if (!parkingData.in_time) {
-    parkingData.in_time = new Date(new Date().toLocaleString('en', { timeZone: config.timeZone }));
+    parkingData.in_time = new Date(new Date().toLocaleString('sv', { timeZone: config.timeZone }));
+  } else {
+    parkingData.in_time += '+07';
   }
 
-  return parking.create({
+  return Parking.create({
     vech_type: parkingData.vech_type,
     vech_num: parkingData.vech_num,
     in_time: parkingData.in_time,
   });
 };
 
-module.exports = { newParking };
+/**
+ * Unregister existing parking
+ * @param {Object} parkingData
+ * @returns {Promise<Parking>}
+ */
+const unregisterParking = async (parkingData) => {
+  const car = await findVehicleInParking(parkingData.vech_num);
+  if (car == null) {
+    throw new ApiError(httpStatus.NOT_FOUND);
+  }
+
+  car.out_time = new Date(new Date().toLocaleString('sv', { timeZone: config.timeZone }));
+  car.total = await parkingPaymentCalculator(car.in_time, car.out_time, car.vech_type);
+
+  await car.save();
+
+  return car;
+};
+
+module.exports = {
+  findVehicleInParking,
+  newParking,
+  unregisterParking,
+};
