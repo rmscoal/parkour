988b9c36a1dad828071a60a949e99ae968f8ed79
rmscoal <rifkymanuelsatyana@proton.me>
2023-01-09 23:31:24 +0700

    Modify db config

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,10 +1,8 @@
-// const config = require('../../config/config');
 const logger = require('../../config/logger');
 
 module.exports = {
   development: {
-    // url: config.pg_url,
-    url: 'postgres://rmscoal:password@localhost:5432/parkour_development',
+    url: process.env.PG_URL,
     protocol: 'postgres',
     dialect: 'postgres',
     dialectOption: {
@@ -14,9 +12,23 @@ module.exports = {
     },
     pool: {
       max: 5,
+      /*
+       * Set this value to 0 so connection pool eviction
+       * logic eventually cleans up all connections
+       * in the event of a Lambda function timeout.
+       */
       min: 0,
+      /*
+       * Choose a small enough value that fails fast if
+       * a connection takes too long to be established.
+       */
       acquire: 30000,
-      idle: 10000,
+      /*
+       * Set this value to 0 so connections are eligible
+       * for cleanup immediately after they're returned to
+       * the pool.
+       */
+      idle: 0,
     },
   },
   test: {
