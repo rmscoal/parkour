07644893d4158c523a4d63d9d8c3b064ff62c641
rmscoal <rifkymanuelsatyana@proton.me>
2023-01-10 20:45:31 +0700

    Edit area controllers and tests

--- src/controllers/area.controller.js
+++ src/controllers/area.controller.js
@@ -2,6 +2,14 @@ const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { areaUseCase } = require('../usecases');
 
+/**
+ * Hierarchical structure:
+ * Province > City > District > Village
+ */
+
+/**
+ * Finds all province.
+ */
 const getAllProvince = catchAsync(async (_req, res) => {
   const data = await areaUseCase.findAllProvince();
   res.status(httpStatus.OK).json({
@@ -10,6 +18,9 @@ const getAllProvince = catchAsync(async (_req, res) => {
   });
 });
 
+/**
+ * Finds all cities with given province id.
+ */
 const getAllCitiesWithProvinceId = catchAsync(async (req, res) => {
   const data = await areaUseCase.findCitiesWithProvinceId(req.params.id);
   res.status(httpStatus.OK).json({
@@ -18,6 +29,9 @@ const getAllCitiesWithProvinceId = catchAsync(async (req, res) => {
   });
 });
 
+/**
+ * Finds all districts with given city id.
+ */
 const getAllDistrictsWithCityId = catchAsync(async (req, res) => {
   const data = await areaUseCase.findDistrictsWithCityId(req.params.id);
   res.status(httpStatus.OK).json({
@@ -26,6 +40,9 @@ const getAllDistrictsWithCityId = catchAsync(async (req, res) => {
   });
 });
 
+/**
+ * Finds all villages with given district id.
+ */
 const getAllVillagesWithDistrictId = catchAsync(async (req, res) => {
   const data = await areaUseCase.findVillagesWithDistrictId(req.params.id);
   res.status(httpStatus.OK).json({
--- test/unit/usecase/area.test.js
+++ test/unit/usecase/area.test.js
@@ -11,7 +11,7 @@ chai.use(chaiAsPromised);
 describe('Area Usecases', () => {
   describe('Find all cities with given province id', () => {
     it('Should returned the correct cities', (done) => {
-      areaUseCase.findCityWithProvinceId(1).then((data) => {
+      areaUseCase.findCitiesWithProvinceId(1).then((data) => {
         data.forEach((city) => {
           expect(city.province_id).equal(1, 'Should have province_id === 1');
         });
@@ -24,7 +24,7 @@ describe('Area Usecases', () => {
 
   describe('Find all districts with given city id', () => {
     it('Should returned the correct districts', (done) => {
-      areaUseCase.findDistrictWithCityId(1).then((data) => {
+      areaUseCase.findDistrictsWithCityId(1).then((data) => {
         data.forEach((district) => {
           expect(district.city_id).equal(1, 'Should have city_id === 1');
         });
@@ -37,7 +37,7 @@ describe('Area Usecases', () => {
 
   describe('Find all villages with given district id', () => {
     it('Should returned the correct villages', (done) => {
-      areaUseCase.findVillageWithDistrictId(1).then((data) => {
+      areaUseCase.findVillagesWithDistrictId(1).then((data) => {
         data.forEach((village) => {
           expect(village.district_id).equal(1, 'Should have district_id === 1');
         });
